e299674624493b8031f56d0322e233b428204acf
inesferreira-90 <124178356+inesferreira-90@users.noreply.github.com>
2023-02-01 20:36:25 +0000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,786 @@
+var dataSet = [
+	{
+		"programme": {
+			"name": "Licenciatura Design",
+			"duration": 3,
+			"courses_number": 146,
+			"teachers_number": 12,
+			"students_number": 198,
+		},
+		
+		"course_name": "Design Global",
+		"teacher_name": "Camilla Patrick",
+		"teacher_birth": "21/32/1970",
+		
+		"salary": "820.43€",
+
+		"student": {
+			"name": "Indira Petty",
+			"birth": "21/35/2003",
+			"number": "UYL38GR",
+			"registrations": 71,
+		},
+		
+		"semester": "2º Semestre",
+		"ects": 4
+	},
+	{
+		"programme": {
+			"name": "Lorem ipsum",
+			"duration": 4,
+			"courses_number": 101,
+			"teachers_number": 10,
+			"students_number": 196,
+		},
+		
+		"course_name": "Nullam lobortis",
+
+		"teacher_name": "Colorado Lara",
+		"teacher_birth": "02/41/1987",
+		
+		"salary": "843.06€",
+
+		"student": {
+			"name": "Ina Miles",
+			"birth": "19/33/1999",
+			"number": "YRH36NU",
+			"registrations": 43,
+		},
+
+		"semester": "3º Semestre",
+		"ects": 5
+	},
+	{
+		"programme": {
+			"name": "Lorem",
+			"duration": 5,
+			"courses_number": 104,
+			"teachers_number": 19,
+			"students_number": 28,
+		},
+		
+		"course_name": "Nec Ante",
+		"teacher_name": "Gemma Pena",
+		"teacher_birth": "12/59/1989",
+		
+		"salary": "789.68€",
+		"student": {
+			"name": "Jeanette Fitzpatrick",
+			"birth": "27/38/1996",
+			"number": "ZMX65EM",
+			"registrations": 62,
+		},
+		
+		"semester": "6º Semestre",
+		"ects": 6
+	},
+	{
+		"programme": {
+			"name": "Lorem",
+			"duration": 4,
+			"courses_number": 195,
+			"teachers_number": 10,
+			"students_number": 116,
+		},
+		
+		"course_name": "Vivamus non arcu",
+		"teacher_name": "Maile Madden",
+		"teacher_birth": "19/04/1947",
+		"salary": "789.64€",
+		"student": {
+			"name": "Delilah Cherry",
+			"birth": "25/18/2002",
+			"number": "YHY55EK",
+			"registrations": 56,
+		},
+		
+		"semester": "6º Semestre",
+		"ects": 4
+	},
+	{
+		"programme": {
+			"name": "Lorem",
+			"duration": 4,
+			"courses_number": 30,
+			"teachers_number": 6,
+			"students_number": 54,
+		},
+		
+		"course_name": "Egestas",
+		"teacher_name": "Mechelle Mclaughlin",
+		"teacher_birth": "13/15/1984",
+		"salary": "604.19€",
+
+		"student": {
+			"name": "Jesse Carr",
+			"birth": "26/19/1999",
+			"number": "DBK29ON",
+			"registrations": 146,
+		},
+
+		"semester": "6º Semestre",
+		"ects": 3
+	},
+	{
+		"programme": {
+			"name": "Lorem ipsum",
+			"duration": 3,
+			"courses_number": 182,
+			"teachers_number": 19,
+			"students_number": 141,
+		},
+		
+		"course_name": "Augue malesuada malesuada",
+		"teacher_name": "Jana Phelps",
+		"teacher_birth": "12/09/1944",
+		"salary": "982.84€",
+
+		"student": {
+			"name": "Grant Washington",
+			"birth": "31/25/1991",
+			"number": "VPH55AP",
+			"registrations": 80,
+		},
+
+		"semester": "4º Semestre",
+		"ects": 4
+	},
+	{
+		"programme": {
+			"name": "Lorem",
+			"duration": 3,
+			"courses_number": 199,
+			"teachers_number": 19,
+			"students_number": 152,
+		},
+		
+		"course_name": "Posuere vulputate",
+		"teacher_name": "Britanni Pollard",
+		"teacher_birth": "09/52/1959",
+		"salary": "697.25€",
+
+		"student": {
+			"name": "Mufutau Osborne",
+			"birth": "21/26/1998",
+			"number": "ZSW35LW",
+			"registrations": 140,
+		},
+
+		"semester": "1º Semestre",
+		"ects": 6
+	},
+	{
+		"programme": {
+			"name": "Lorem",
+			"duration": 4,
+			"courses_number": 73,
+			"teachers_number": 15,
+			"students_number": 153,
+		},
+		
+		"course_name": "Suscipit",
+		"teacher_name": "Dalton Mason",
+		"teacher_birth": "10/22/1947",
+		"salary": "925.36€",
+
+		"student": {
+			"name": "Dane Ballard",
+			"birth": "11/25/1996",
+			"number": "SYW74KX",
+			"registrations": 90,
+		},
+
+		"semester": "5º Semestre",
+		"ects": 3
+	},
+	{
+		"programme": {
+			"name": "Lorem",
+			"duration": 3,
+			"courses_number": 32,
+			"teachers_number": 12,
+			"students_number": 19,
+		},
+		
+		"course_name": "Sit Amet",
+		"teacher_name": "Hakeem Nieves",
+		"teacher_birth": "06/07/1956",
+		"salary": "915.33€",
+
+		"student": {
+			"name": "Marny Hester",
+			"birth": "17/57/1996",
+			"number": "SHT81JC",
+			"registrations": 88,
+		},
+
+		"semester": "1º Semestre",
+		"ects": 5
+	},
+	{
+		"programme": {
+			"name": "Lorem ipsum",
+			"duration": 4,
+			"courses_number": 10,
+			"teachers_number": 13,
+			"students_number": 39,
+		},
+		
+		"course_name": "Vitae Aliquam Eros",
+		"teacher_name": "Zorita Mejia",
+		"teacher_birth": "20/00/1982",
+		"salary": "864.46€",
+
+		"student": {
+			"name": "Knox Morton",
+			"birth": "29/06/1992",
+			"number": "NDW68XN",
+			"registrations": 155,
+		},
+
+		"semester": "2º Semestre",
+		"ects": 4
+	},
+	{
+		"programme": {
+			"name": "Lorem ipsum",
+			"duration": 5,
+			"courses_number": 137,
+			"teachers_number": 20,
+			"students_number": 186,
+		},
+
+		"course_name": "Posuere At",
+		"teacher_name": "Clio Mitchell",
+		"teacher_birth": "11/40/1963",
+		"salary": "849.44€",
+
+		"student": {
+			"name": "Audra Roy",
+			"birth": "15/31/1997",
+			"number": "YZQ11IZ",
+			"registrations": 146,
+		},
+
+		"semester": "5º Semestre",
+		"ects": 6
+	},
+	{
+		"programme": {
+			"name": "Lorem ipsum",
+			"duration": 5,
+			"courses_number": 41,
+			"teachers_number": 14,
+			"students_number": 27,
+		},
+
+		"course_name": "Libero Dui",
+		"teacher_name": "Yetta Pope",
+		"teacher_birth": "08/05/1984",
+		"salary": "951.72€",
+
+		"student": {
+			"name": "Cadman Day",
+			"birth": "31/40/1997",
+			"number": "OAM32HP",
+			"registrations": 33,
+		},
+
+		"semester": "3º Semestre",
+		"ects": 6
+	},
+	{
+		"programme": {
+			"name": "Lorem",
+			"duration": 5,
+			"courses_number": 88,
+			"teachers_number": 19,
+			"students_number": 133,
+		},
+
+		"course_name": "Dolor Sit",
+		"teacher_name": "Shoshana Cortez",
+		"teacher_birth": "01/47/1981",
+		"salary": "905.49€",
+
+		"student": {
+			"name": "Roth Bowen",
+			"birth": "17/51/1992",
+			"number": "EKO64GX",
+			"registrations": 154,
+		},
+
+		"semester": "2º Semestre",
+		"ects": 3
+	},
+	{
+		"programme": {
+			"name": "Lorem ipsum",
+			"duration": 2,
+			"courses_number": 144,
+			"teachers_number": 14,
+			"students_number": 47,
+		},
+
+		"course_name": "Erat Vitae Risus",
+		"teacher_name": "Gannon Atkinson",
+		"teacher_birth": "31/42/1957",
+		"salary": "938.44€",
+
+		"student": {
+			"name": "Kermit Bauer",
+			"birth": "10/03/1990",
+			"number": "ZLX73RK",
+			"registrations": 150,
+		},
+
+		"semester": "5º Semestre",
+		"ects": 5
+	},
+	{
+		"programme": {
+			"name": "Lorem",
+			"duration": 5,
+			"courses_number": 113,
+			"teachers_number": 15,
+			"students_number": 107,
+		},
+
+		"course_name": "Vestibulum",
+		"teacher_name": "Bertha Hudson",
+		"teacher_birth": "03/41/1942",
+		"salary": "863.33€",
+
+
+		"student": {
+			"name": "Devin Griffith",
+			"birth": "03/50/1990",
+			"number": "UNI59DX",
+			"registrations": 27,
+		},
+
+		"semester": "5º Semestre",
+		"ects": 4
+	},
+	{
+		"programme": {
+			"name": "Lorem ipsum",
+			"duration": 3,
+			"courses_number": 19,
+			"teachers_number": 10,
+			"students_number": 22,
+		},
+
+		"course_name": "Phasellus elit",
+		"teacher_name": "Quintessa Shields",
+		"teacher_birth": "04/20/1943",
+		"salary": "968.63€",
+
+		"student": {
+			"name": "Felix Mccarthy",
+			"birth": "01/08/1999",
+			"number": "QKR13UM",
+			"registrations": 139,
+		},
+
+		"semester": "2º Semestre",
+		"ects": 5
+	},
+	{
+		"programme": {
+			"name": "Lorem",
+			"duration": 3,
+			"courses_number": 20,
+			"teachers_number": 14,
+			"students_number": 169,
+		},
+		"course_name": "Morbi",
+		"teacher_name": "Rigel Allison",
+		"teacher_birth": "11/23/1970",
+		"salary": "903.95€",
+
+		"student": {
+			"name": "Nevada Sears",
+			"birth": "15/15/1992",
+			"number": "HBL78UU",
+			"registrations": 171,
+		},
+
+		"semester": "2º Semestre",
+		"ects": 6
+	},
+	{
+		"programme": {
+			"name": "Lorem",
+			"duration": 4,
+			"courses_number": 50,
+			"teachers_number": 12,
+			"students_number": 129,
+		},
+
+		"course_name": "Pellentesque",
+		"teacher_name": "Kiona Dunlap",
+		"teacher_birth": "18/58/1973",
+		"salary": "547.53€",
+
+		"student": {
+			"name": "Drew Ashley",
+			"birth": "22/45/1999",
+			"number": "ODN53CY",
+			"registrations": 65,
+		},
+
+		"semester": "1º Semestre",
+		"ects": 4
+	},
+	{
+		"programme": {
+			"name": "Lorem",
+			"duration": 3,
+			"courses_number": 172,
+			"teachers_number": 11,
+			"students_number": 26,
+		},
+
+		"course_name": "Est Arcu",
+		"teacher_name": "Malik Welch",
+		"teacher_birth": "10/09/1964",
+		"salary": "998.52€",
+
+		"student": {
+			"name": "Luke Mccray",
+			"birth": "04/42/2002",
+			"number": "DWX95TW",
+			"registrations": 74,
+		},
+
+		"semester": "5º Semestre",
+		"ects": 5
+	},
+	{
+		"programme": {
+			"name": "Lorem ipsum",
+			"duration": 2,
+			"courses_number": 154,
+			"teachers_number": 15,
+			"students_number": 34,
+		},
+
+		"course_name": "Mi Tempor",
+		"teacher_name": "Cora Bell",
+		"teacher_birth": "27/10/1976",
+		"salary": "514.56€",
+
+		"student": {
+			"name": "Chanda Galloway",
+			"birth": "11/31/1995",
+			"number": "SKW53RR",
+			"registrations": 63,
+		},
+
+		"semester": "2º Semestre",
+		"ects": 5
+	},
+	{
+		"programme": {
+			"name": "Lorem ipsum",
+			"duration": 5,
+			"courses_number": 101,
+			"teachers_number": 14,
+			"students_number": 148,
+		},
+
+		"course_name": "Pellentesque habitant",
+		"teacher_name": "Oprah Jones",
+		"teacher_birth": "29/20/1975",
+		"salary": "984.60€",
+
+		"student": {
+			"name": "Rowan English",
+			"birth": "10/47/1993",
+			"number": "QBX98CT",
+			"registrations": 112,
+		},
+
+		"semester": "6º Semestre",
+		"ects": 5
+	},
+	{
+		"programme": {
+			"name": "Lorem",
+			"duration": 4,
+			"courses_number": 175,
+			"teachers_number": 15,
+			"students_number": 137,
+		},
+
+		"course_name": "Amet Dapibus",
+		"teacher_name": "Caleb Boyer",
+		"teacher_birth": "09/18/1942",
+		"salary": "604.67€",
+
+		"student": {
+			"name": "Hedy Richmond",
+			"birth": "18/26/1998",
+			"number": "JLJ36IV",
+			"registrations": 119,
+		},
+
+		"semester": "2º Semestre",
+		"ects": 6
+	},
+	{
+		"programme": {
+			"name": "Lorem",
+			"duration": 2,
+			"courses_number": 169,
+			"teachers_number": 14,
+			"students_number": 138,
+		},
+
+		"course_name": "Dolor Quam Elementum",
+		"teacher_name": "Tanek Franco",
+		"teacher_birth": "01/44/1962",
+		"salary": "713.35€",
+
+		"student": {
+			"name": "Dorothy Greene",
+			"birth": "19/37/2001",
+			"number": "QJK84QP",
+			"registrations": 185,
+		},
+
+		"semester": "3º Semestre",
+		"ects": 5
+	},
+	{
+		"programme": {
+			"name": "Lorem ipsum",
+			"duration": 4,
+			"courses_number": 82,
+			"teachers_number": 17,
+			"students_number": 97,
+		},
+
+		"course_name": "Purus",
+		"teacher_name": "Castor Franco",
+		"teacher_birth": "26/03/1956",
+		"salary": "814.13€",
+
+		"student": {
+			"name": "Erica Hoffman",
+			"birth": "21/33/1992",
+			"number": "LYC03WE",
+			"registrations": 23,
+		},
+
+		"semester": "6º Semestre",
+		"ects": 5
+	},
+	{
+		"programme": {
+			"name": "Lorem ipsum",
+			"duration": 3,
+			"courses_number": 67,
+			"teachers_number": 13,
+			"students_number": 42,
+		},
+
+		"course_name": "Nisi al",
+		"teacher_name": "Sigourney Olsen",
+		"teacher_birth": "21/31/1964",
+		"salary": "924.49€",
+
+		"student": {
+			"name": "Xantha Robertson",
+			"birth": "13/16/1991",
+			"number": "KKV77OS",
+			"registrations": 29,
+		},
+
+		"semester": "4º Semestre",
+		"ects": 5
+	}
+];
+
+var dataGrades = [
+	{
+		"course_name": "Design Global",
+		"grade": 17
+	},
+	{
+		"course_name": "Nisi al",
+		"grade": 17
+	},
+	{
+		"course_name": "Purus",
+		"grade": 5
+	},
+	{
+		"course_name": "Dolor Quam Elementum",
+		"grade": 18
+	},
+	{
+		"course_name": "Pellentesque",
+		"grade": 19
+	},
+	{
+		"course_name": "Erat Vitae Risus",
+		"grade": 14
+	},
+	{
+		"course_name": "Nisi al di",
+		"grade": 7
+	},
+	{
+		"course_name": "Morbi",
+		"grade": 20
+	},
+	{
+		"course_name": "Erat Vitae ",
+		"grade": 13
+	},
+	{
+		"course_name": "Lorem Ipsum ",
+		"grade": 16
+	},
+	{
+		"course_name": "Suscipit",
+		"grade": 15
+	},
+	{
+		"course_name": "Sit Amet",
+		"grade": 8
+	}
+]
+
+
+
+$('#programmeCourses').DataTable({
+	"order": false,
+	language: {
+		url: 'https://cdn.datatables.net/plug-ins/1.13.1/i18n/pt-PT.json',
+		"paginate": {
+			"first": "",
+			"previous": "",
+			"next": "",
+			"last": ""
+		},
+	},
+	searching: false,
+	lengthChange:false,
+	order: [[1, 'asc']],
+	responsive: true,
+	data: dataSet,
+	columns: [
+		{ data: 'course_name', title: 'Disciplina' },
+		{ data: 'semester', title: 'Semestre'  },
+		{ data: 'ects', title: 'ECTS'  },
+		{ data: 'teacher_name', title: 'Professor'  },
+	],
+});
+
+$('#programmes').DataTable({
+	"order": false,
+	language: {
+		url: 'https://cdn.datatables.net/plug-ins/1.13.1/i18n/pt-PT.json',
+		"paginate": {
+			"first": "",
+			"previous": "",
+			"next": "",
+			"last": ""
+		},
+	},
+	responsive: true,
+	"columnDefs": [
+		{ responsivePriority: -2, targets: 5 },
+		{ responsivePriority: 10001, targets: 4 },
+	],
+	data: dataSet,
+	columns: [
+		{ data: 'programme.name', title: 'Nome do Curso' },
+		{ data: 'programme.duration', title: 'Duração'  },
+		{ data: 'programme.courses_number', title: 'Disciplinas'  },
+		{ data: 'programme.teachers_number', title: 'Professores'  },
+		{ data: 'programme.students_number', title: 'Alunos inscritos'  },
+		{ responsivePriority: 1, "defaultContent":"<a href='programme-detail.html'><div class='table-row__action'><span class='material-symbols-outlined action-icon'>visibility</span></div></a>"},
+	],
+});
+$('#courses').DataTable({
+	"order": false,
+	language: {
+		url: 'https://cdn.datatables.net/plug-ins/1.13.1/i18n/pt-PT.json',
+		"paginate": {
+			"first": "",
+			"previous": "",
+			"next": "",
+			"last": ""
+		},
+	},
+	data: dataSet,
+	columns: [
+		{ data: 'course_name', title: 'Nome da Disciplina' },
+		{ data: 'programme.name', title: 'Cursos'  },
+		{ data: 'teacher_name', title: 'Professor'  },
+	],
+});
+
+$('#teachers').DataTable({
+	"order": false,
+	language: {
+		url: 'https://cdn.datatables.net/plug-ins/1.13.1/i18n/pt-PT.json',
+		"paginate": {
+			"first": "",
+			"previous": "",
+			"next": "",
+			"last": ""
+		},
+	},
+	data: dataSet,
+	columns: [
+		{ data: 'teacher_name', title: 'Nome' },
+		{ data: 'teacher_birth', title: 'Data de Nascimento'  },
+		{ data: 'salary', title: 'Salário'  },
+		{ data: 'programme.teachers_number', title: 'Cursos'  },
+	],
+});
+
+$('#students').DataTable({
+	"order": false,
+	language: {
+		url: 'https://cdn.datatables.net/plug-ins/1.13.1/i18n/pt-PT.json',
+		"paginate": {
+			"first": "",
+			"previous": "",
+			"next": "",
+			"last": ""
+		},
+	},
+	data: dataSet,
+	columns: [
+		{ data: 'student.number', title: 'Nuúmero de Mareicula' },
+		{ data: 'student.name', title: 'Nome'  },
+		{ data: 'student.birth', title: 'Data de Nascimento'  },
+		{ data: 'programme.name', title: 'Curso'  },
+		{ "defaultContent":"<a href='student-detail.html'><div class='table-row__action'><span class='material-symbols-outlined action-icon'>visibility</span></div></a>"},
+	],
+});
+
+$('#studentGrade').DataTable({
+	"order": false,
+	language: {
+		url: 'https://cdn.datatables.net/plug-ins/1.13.1/i18n/pt-PT.json',
+		"paginate": {
+			"first": "",
+			"previous": "",
+			"next": "",
+			"last": ""
+		},
+	},
+	data: dataGrades,
+	columns: [
+		{ data: 'course_name', title: 'Disciplina' },
+		{ data: 'grade', title: 'Nota'  },
+	],
+});
+
